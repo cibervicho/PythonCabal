23743da76a97a9c3d7e1fd1dbe5e8fcaa053711a
Renich Bon Ciric <renich@woralelandia.com>
2010-06-17 18:10:58 -0500

    Buscar punto en vez de comparar on 0.0
    usando str.find('.') ahora

--- clase_01/tools.py
+++ clase_01/tools.py
@@ -12,7 +12,7 @@ def ponComa( numero ):
     fraccion = numero - entero
     cadena = str( numero )
 
-    if 0.0 != fraccion:
+    if cadena.find('.'): 
         indice = len( str( entero ) )
     else:
         indice = len( cadena )

8ec7913a5c25405ff22fb5c0f689ce98a50155cb
Renich Bon Ciric <renich@woralelandia.com>
2010-06-17 18:53:56 -0500

    Cambié al uso de partition
    eliminé el if y fue más fácil todavía

--- clase_01/tools.py
+++ clase_01/tools.py
@@ -7,22 +7,22 @@ def ponComa( numero ):
     '''Regresa numero como cadena con comas.
 
     numero es un entero
-    no maneja signo ni punto decimal.'''
-    entero = int( numero )
-    fraccion = numero - entero
-    cadena = str( numero )
-
-    if cadena.find('.'): 
-        indice = len( str( entero ) )
-    else:
-        indice = len( cadena )
+    no maneja signo'''
+    particion = str( numero ).partition('.')
+    cadena = particion[0]
+    indice = len( cadena )
 
     while indice > 3:
+        '''Restar 3 al indice'''
         indice = indice - 3
+        
+        '''Agregar coma'''
         cadena = cadena[ :indice ] +  ',' + cadena[ indice: ]
+        
     
-    return cadena
+    '''Regresar resultado con todo y parte decimal'''
+    return cadena + particion[1] + particion[2]
 
 if __name__ == '__main__':
-    for datoDePrueba in [ 0, 12, 123, 1234, 12345, 123456, 1234567, 19234.3498 ]:
+    for datoDePrueba in [ 0, 12, 123, 1234, 12345, 123456, 1234567, 1234.5678 ]:
         print ( datoDePrueba , ponComa( datoDePrueba ) )

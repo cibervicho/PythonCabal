54584b04e0546b83679eaec634e63d11ce803289
Renich Bon Ciric <renich@woralelandia.com>
2010-06-17 03:21:53 -0500

    Commit inicial

---
+++ tools.py
@@ -0,0 +1,16 @@
+def ponComa( numero ):
+    '''Regresa numero como cadena con comas.
+
+    numero es un entero
+    no maneja signo ni punto decimal.'''
+
+    cadena = str( numero )
+    indice = len(cadena)
+    while indice > 3:
+        indice = indice - 3
+        cadena = cadena[ :indice ] +  ',' + cadena[ indice: ]
+    return cadena
+
+if __name__ == '__main__':
+    for datoDePrueba in [ 0, 12, 123, 1234, 12345, 123456, 1234567 ]:
+        print ( datoDePrueba , ponComa( datoDePrueba ) )

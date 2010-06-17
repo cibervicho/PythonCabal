e429f90decb7f154aa64aa98b6ad5576ffb66c77
Renich Bon Ciric <renich@woralelandia.com>
2010-06-17 14:58:32 -0500

    Agregué soporte para puntos decimales

--- tools.py
+++ tools.py
@@ -8,14 +8,21 @@ def ponComa( numero ):
 
     numero es un entero
     no maneja signo ni punto decimal.'''
-
+    entero = int( numero )
+    fraccion = numero % int(numero) if 0 != numero else 0
     cadena = str( numero )
-    indice = len(cadena)
+
+    if fraccion != 0.0:
+        indice = len( str( entero ) )
+    else:
+        indice = len( cadena )
+
     while indice > 3:
         indice = indice - 3
         cadena = cadena[ :indice ] +  ',' + cadena[ indice: ]
+    
     return cadena
 
 if __name__ == '__main__':
-    for datoDePrueba in [ 0, 12, 123, 1234, 12345, 123456, 1234567 ]:
+    for datoDePrueba in [ 1323.23, 0, 12, 123, 1234, 12345, 123456, 1234567, 19234.3498 ]:
         print ( datoDePrueba , ponComa( datoDePrueba ) )
